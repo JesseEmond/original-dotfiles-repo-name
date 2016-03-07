688e17cb731813bf6ccc2d00091089617c10d6d8
Jesse Emond <emond.jesse@gmail.com>
2016-03-07 03:54:13 -0500

    tmux: initial serious conf & dev script for setup

---
+++ scripts/dev.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+alias tmux="tmux -2"
+
+# attach to it if possible
+tmux attach-session -t 'dev' && exit $?
+
+# create it otherwise
+cd ~/projects/$1
+tmux new-session -d -s 'dev' -n 'src' 'vim .'
+tmux split-window -v -p 30
+tmux new-window -n 'sh'
+tmux split-window -v
+tmux select-window -t 1
+tmux select-pane -t 1
+tmux attach-session -t 'dev'
